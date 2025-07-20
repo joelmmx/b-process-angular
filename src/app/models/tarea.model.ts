c21462a49987d9112f14a2df92b7819dec003d02
Joel Mercado <joelmmx@gmail.com>
2025-07-19 23:27:20 -0600

    Add toggle and display logic for completed tasks & Add Tarea model and update component to handle completed tasks

---
+++ src/app/models/tarea.model.ts
@@ -0,0 +1,4 @@
+export interface Tarea {
+  descripcion: string;
+  completado: boolean;
+}
--- src/app/tareas/tareas.component.ts
+++ src/app/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../models/tarea.model';
 
 @Component({
   selector: 'app-tareas',
@@ -8,13 +9,20 @@ import { Component } from '@angular/core';
 export class TareasComponent {
   titulo = 'Lista de tareas';
   nuevaTarea = '';
-  tareas: string[] = [];
+  tareas: Tarea[] = [];
 
   agregarTarea() {
-    if (this.nuevaTarea.trim()) {
-      this.tareas.push(this.nuevaTarea.trim());
-      this.nuevaTarea = '';
-    }
+  if (this.nuevaTarea.trim()) {
+    this.tareas.push({
+      descripcion: this.nuevaTarea.trim(),
+      completado: false
+    });
+    this.nuevaTarea = '';
+  }
+}
+
+  toggleTareaCompletada(index: number) {
+    this.tareas[index].completado = !this.tareas[index].completado;
   }
 
   eliminarTarea(index: number) {
